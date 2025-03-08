ccdd717720d43be86ac890ffaa6ef93ca8f04f45
K1dou <hique1276@gmail.com>
2025-03-08 13:37:49 -0300

    project hr-worker created

---
+++ hr-worker/src/test/java/com/kidou/hr_worker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidou.hr_worker;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Override
+	public String toString() {
+		return "HrWorkerApplicationTests []";
+	}
+
+}
