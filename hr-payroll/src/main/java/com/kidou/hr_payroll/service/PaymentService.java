d88c3425ec932d2f4e45bbec18e2ca200418d47b
K1dou <hique1276@gmail.com>
2025-03-08 15:32:25 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/kidou/hr_payroll/service/PaymentService.java
@@ -0,0 +1,14 @@
+package com.kidou.hr_payroll.service;
+
+import org.springframework.stereotype.Service;
+
+import com.kidou.hr_payroll.model.Payment;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days) {
+        return new Payment("Bob", 200.0, days);
+    }
+
+}
