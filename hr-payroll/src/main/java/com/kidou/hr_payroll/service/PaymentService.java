c45e06557feee25f60ea26267891c1955439e618
K1dou <hique1276@gmail.com>
2025-03-08 22:45:51 -0300

    Resilience4j falut tolerance

--- hr-payroll/src/main/java/com/kidou/hr_payroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/kidou/hr_payroll/service/PaymentService.java
@@ -7,16 +7,25 @@ import com.kidou.hr_payroll.feignclients.WorkerFeignClient;
 import com.kidou.hr_payroll.model.Payment;
 import com.kidou.hr_payroll.model.Worker;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
 public class PaymentService {
 
     @Autowired
     private WorkerFeignClient workerFeignClient;
 
+    @CircuitBreaker(name = "hr-payroll", fallbackMethod = "getPaymentAlternative")
     public Payment getPayment(long workerId, int days) {
 
         Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
+    }
+
+    public Payment getPaymentAlternative(Long workerId, int days) {
+        Payment payment = new Payment("Marcelo", 200.0, 5);
+
+        return payment;
     }
 
 }
